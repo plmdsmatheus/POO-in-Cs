cea75f149849fc4021422866177b7eec2bce2859
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2022-05-25 14:32:20 +0000

    Aula 25/05

--- Aula_06/lista02.cs
+++ Aula_06/lista02.cs
@@ -11,6 +11,8 @@ class Program{
     Console.WriteLine($"Saldo = {x.GetSaldo()}");
     x.sacar(100);
     Console.WriteLine($"Saldo = {x.GetSaldo()}");
+    Console.WriteLine(x);
+    
     
     
     }
@@ -25,28 +27,31 @@ class Program{
     public string titular, numero;
     private double saldo;
     public void Depositar(double valor){
-      if (valor > 0) saldo += valor;
+      if (valor > 0) this.saldo += valor;
       // else Console.WriteLine("Valor deve ser maior que zero");
     }
     public void sacar(double valor){
-      if (valor <= saldo && valor > 0) saldo -= valor;
+      if (valor <= saldo && valor > 0) this.saldo -= valor;
       // else Console.WriteLine("Saldo insuficiente ou valor depositado invalido");
     }
     public double GetSaldo(){
-      return saldo;
+      return this.saldo;
     }
     public void SetTitular(string s) {
-      if (s.IndexOf(' ') != -1 ) titular = s;
+      if (s.IndexOf(' ') != -1 ) this.titular = s;
       // else Console.WriteLine("Nome é invalido");
     }
       public void SetNumero(string s) {
-      if (s.IndexOf('-') != -1 ) numero = s;
+      if (s.IndexOf('-') != -1 ) this.numero = s;
       // else Console.WriteLine("numero é invalido");
       }
     public string GetTitular(){
-      return titular;
+      return this.titular;
     }
     public string GetNumero(){
-    return numero;
+    return this.numero;
+    }
+    public override string ToString() {
+      return $"Titular = {titular}, Número da conta = {numero}, Saldo = {saldo}";
     }
   }
